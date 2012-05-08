6a9101bd52b0456a76a840efee8f25b21bc95393
Kim Tore Jensen <kim.tore@freecode.no>
2012-05-08 11:20:01 +0200

    Use vi bindings.

--- kbash.sh
+++ kbash.sh
@@ -12,6 +12,9 @@ fi
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
+# use vi bindings
+set -o vi
+
 # don't put duplicate lines in the history. See bash(1) for more options
 export HISTCONTROL=ignoredups
 
